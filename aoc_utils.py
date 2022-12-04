9a0f56e89b926c299a1b0b5c9a56801edcee4a91
Golden <68432299+CrazyCoderK@users.noreply.github.com>
2022-12-04 14:26:59 +0000

    Add files via upload

---
+++ aoc_utils.py
@@ -0,0 +1,38 @@
+import inspect
+import os
+
+def input_int_list():
+    """parse input into a list of ints"""
+    with open("AoC/input.txt", "r") as file:
+        return [int(line.rstrip()) for line in file]
+
+
+def input_string_list():
+    """parse input into a list of strings"""
+    with open("AoC/input.txt", "r") as file:
+        return [line.rstrip() for line in file]
+
+
+def input_block_list():
+    with open("AoC/input.txt") as f:
+        data = f.read().splitlines()
+    arr, lis = [], []
+    for count, val in enumerate(data):
+        if val != "":
+            lis.append(val)
+        else:
+            arr.append(lis)
+            lis = []
+    return arr
+
+def filter_empty(li):
+    """remove empty entries (e.g. when splitting a string)"""
+    return list(filter(None, li))
+
+def remove_comma_list(s):
+    return [int(x) for x in s.split(",")]
+
+def make_string_int(l):
+    for i in range(len(l)):
+        l[i] = int(l[i])
+    return l
